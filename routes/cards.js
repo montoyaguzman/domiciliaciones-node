c9a5b39aceaffa3b69e3317eae03463dbda947c1
MarioSalinas <mario_1605@live.com.mx>
2019-11-10 20:19:21 -0600

    feature/card services with connection to mlab

--- routes/cards.js
+++ routes/cards.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const CardsService = require("../services/cards");
 const { cardsServiceMock } = require("../utils/mocks/cards");
 
 function cardsApi(app) {
   const router = express.Router();
   app.use("/api/cards", router);
 
+  const cardsService = new CardsService();
+
   router.get("/", async (req, res) => {
     try {
-      const serviceResponse = await cardsServiceMock.prototype.getCard();
+      const serviceResponse = await cardsService.getCards();
+      res.status(200).json({
+        data: serviceResponse,
+        message: "Card listed"
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  router.get("/:cardId", async (req, res) => {
+    const { cardId } = req.params;
+    try {
+      const serviceResponse = await cardsService.getCard(cardId);
       res.status(200).json({
         data: serviceResponse,
         message: "Card listed"
@@ -18,8 +34,10 @@ function cardsApi(app) {
   });
 
   router.post("/", async (req, res) => {
+    const { body: card } = req;
+
     try {
-      const serviceResponse = await cardsServiceMock.prototype.createCard();
+      const serviceResponse = await cardsService.createCard(card);
       res.status(201).json({
         data: serviceResponse,
         message: "Card created"
@@ -29,9 +47,15 @@ function cardsApi(app) {
     }
   });
 
-  router.put("/", async (req, res) => {
+  router.put("/:cardId", async (req, res) => {
+    const { cardId } = req.params;
+    const { body: updatedCard } = req;
+
     try {
-      const serviceResponse = await cardsServiceMock.prototype.updateCard();
+      const serviceResponse = await cardsService.updateCard(
+        cardId,
+        updatedCard
+      );
       res.status(201).json({
         data: serviceResponse,
         message: "Card updated"
@@ -41,12 +65,14 @@ function cardsApi(app) {
     }
   });
 
-  router.delete("/", async (req, res) => {
+  router.delete("/:cardId", async (req, res) => {
+    const { cardId } = req.params;
+
     try {
-      const serviceResponse = await cardsServiceMock.prototype.deleteCard();
+      const serviceResponse = await cardsService.deleteCard(cardId);
       res.status(200).json({
         data: serviceResponse,
-        message: "Card updated"
+        message: "Card deleted"
       });
     } catch (error) {
       console.log(error);

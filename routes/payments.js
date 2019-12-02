6bcdcc69d5a88bf160ac2990678df7a2ab97dfb3
MarioSalinas <mario_1605@live.com.mx>
2019-12-02 17:01:32 -0600

    feature/verify-jwt

--- routes/payments.js
+++ routes/payments.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const passport = require("passport");
+
+const validationHandler = require("../utils/middleware/validationHanddler");
+const { paymentsCardSchema } = require("../utils/schemas/payments");
 const PaymentsService = require("../services/payments");
 const { paymentsServiceMock } = require("../utils/mocks/payments");
+
+require("../utils/auth/strategies/jwt");
 
 function paymentsApi(app) {
   const router = express.Router();
@@ -8,72 +14,92 @@ function paymentsApi(app) {
 
   const paymentsService = new PaymentsService();
 
-  router.get("/", async (req, res) => {
-    try {
-      const serviceResponse = await paymentsService.getPayments();
-      res.status(201).json({
-        data: serviceResponse,
-        message: "Payment listed"
-      });
-    } catch (error) {
-      console.log(error);
+  router.get(
+    "/",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      try {
+        const serviceResponse = await paymentsService.getPayments();
+        res.status(201).json({
+          data: serviceResponse,
+          message: "Payment listed"
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
-  });
-  router.get("/:paymentId", async (req, res) => {
-    const { paymentId } = req.params;
-    try {
-      const serviceResponse = await paymentsService.getPayment(paymentId);
-      res.status(201).json({
-        data: serviceResponse,
-        message: "Payment listed"
-      });
-    } catch (error) {
-      console.log(error);
+  );
+  router.get(
+    "/:paymentId",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      const { paymentId } = req.params;
+      try {
+        const serviceResponse = await paymentsService.getPayment(paymentId);
+        res.status(201).json({
+          data: serviceResponse,
+          message: "Payment listed"
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
-  });
+  );
 
-  router.post("/", async (req, res) => {
-    const { body: payment } = req;
-    try {
-      const serviceResponse = await paymentsService.createPayment(payment);
-      res.status(201).json({
-        data: serviceResponse,
-        message: "Payment created"
-      });
-    } catch (error) {
-      console.log(error);
+  router.post(
+    "/",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      const { body: payment } = req;
+      try {
+        const serviceResponse = await paymentsService.createPayment(payment);
+        res.status(201).json({
+          data: serviceResponse,
+          message: "Payment created"
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
-  });
+  );
 
-  router.put("/:paymentId", async (req, res) => {
-    const { paymentId } = req.params;
-    const { body: updatedPayment } = req;
-    try {
-      const serviceResponse = await paymentsService.updatePayment(
-        paymentId,
-        updatedPayment
-      );
-      res.status(200).json({
-        data: serviceResponse,
-        message: "Payment updated"
-      });
-    } catch (error) {
-      console.log(error);
+  router.put(
+    "/:paymentId",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      const { paymentId } = req.params;
+      const { body: updatedPayment } = req;
+      try {
+        const serviceResponse = await paymentsService.updatePayment(
+          paymentId,
+          updatedPayment
+        );
+        res.status(200).json({
+          data: serviceResponse,
+          message: "Payment updated"
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
-  });
+  );
 
-  router.delete("/:paymentId", async (req, res) => {
-    const { paymentId } = req.params;
-    try {
-      const serviceResponse = await paymentsService.deletePayment(paymentId);
-      res.status(200).json({
-        data: serviceResponse,
-        message: "Payment deleted"
-      });
-    } catch (error) {
-      console.log(error);
+  router.delete(
+    "/:paymentId",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      const { paymentId } = req.params;
+      try {
+        const serviceResponse = await paymentsService.deletePayment(paymentId);
+        res.status(200).json({
+          data: serviceResponse,
+          message: "Payment deleted"
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
-  });
+  );
 }
 
 module.exports = paymentsApi;
---
+++ utils/schemas/payments.js
@@ -0,0 +1,24 @@
+const joi = require("@hapi/joi");
+
+const paymentsIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
+
+const paymentsCardSchema = joi.object({
+  idCard: joi
+    .string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required(),
+  idAgreement: joi
+    .string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required(),
+  alias: joi.string(),
+  description: joi.string(),
+  reference: joi.string().required(),
+  paymentDate: joi.string().required(),
+  status: joi.string()
+});
+
+module.exports = {
+  paymentsIdSchema,
+  paymentsCardSchema
+};
